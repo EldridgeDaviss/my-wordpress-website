f4b6a72e87638346fa80b65726b0b94dc9269a2c
EldridgeDaviss <eldridge.davis@students.jmcss.org>
2024-12-17 07:45:45 -0600

    Initial commit

---
+++ wp-content/plugins/kubio/build/utils/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('kubio-editor-data', 'kubio-icons', 'lodash', 'react', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-polyfill'), 'version' => 'a0922ee9570942b71688');
